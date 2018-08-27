a37fd15392c04d12f986408079551d54136d0299
Frederik Rehbach <frederik.re@gmail.com>
2018-08-27 14:56:22 +0200

    Change Test skips

--- tests/testthat/test-GUI.R
+++ tests/testthat/test-GUI.R
@@ -4,6 +4,6 @@ library(shinytest)
 context("Shiny GUI Tests")
 
 test_that("Application works", {
-    skip_on_appveyor()
+    skip_on_cran()
     expect_error(testApp(system.file("appTests",package = "EventDetectGUI")), regexp = NA)
 })
