d2901b01d07fe847147462de9c319dc16cde111c
Frederik Rehbach <frederik.re@gmail.com>
2018-08-27 10:20:42 +0200

    Skip on appveyor

--- tests/testthat/test-GUI.R
+++ tests/testthat/test-GUI.R
@@ -4,5 +4,6 @@ library(shinytest)
 context("Shiny GUI Tests")
 
 test_that("Application works", {
+    skip_on_appveyor()
     expect_error(testApp(system.file("appTests",package = "EventDetectGUI")), regexp = NA)
 })
